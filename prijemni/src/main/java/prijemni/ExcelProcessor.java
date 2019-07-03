dc64297e317d96cd358858c5b3235cf31b93be30
dzoli <bje.no9@gmail.com>
2019-07-03 02:11:57 +0200

    bf

--- prijemni/src/main/java/prijemni/ExcelProcessor.java
+++ prijemni/src/main/java/prijemni/ExcelProcessor.java
@@ -11,33 +11,31 @@ import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellAddress;
 
 public class ExcelProcessor {
-//	String outPath = System.getProperty("user.home");
-//	System.out.println("== out home == " + outPath);
 
 	public static void main(String[] args) {
 		String inPath = args[0];
 		String sfrDel = args[1];
 		Row delRow = null;
-		
+		HSSFWorkbook wb = null;
+
 		try (OutputStream os = new FileOutputStream("workbook.xls")) {
-	        DataFormatter dataFormatter = new DataFormatter();
-			
+			DataFormatter dataFormatter = new DataFormatter();
+
 			FileInputStream fis = new FileInputStream(inPath);
-			HSSFWorkbook wb = new HSSFWorkbook(fis);
+			wb = new HSSFWorkbook(fis);
 			HSSFSheet sheet = wb.getSheet("RangListPMF");
-			Iterator<Row> rows =sheet.rowIterator();	
+			Iterator<Row> rows = sheet.rowIterator();
 			while (rows.hasNext()) {
 				Row row = rows.next();
 				Iterator<Cell> cells = row.cellIterator();
-				
+
 				while (cells.hasNext()) {
 					Cell cell = cells.next();
 					String cellVal = dataFormatter.formatCellValue(cell);
@@ -46,40 +44,72 @@ public class ExcelProcessor {
 					}
 				}
 			}
-			
+
 			System.out.println("== deleting student " + sfrDel + " at row " + delRow.getRowNum());
 			int lastRow = sheet.getLastRowNum();
 			sheet.removeRow(delRow);
 			sheet.shiftRows(delRow.getRowNum(), lastRow, -1);
 			sheet.shiftRows(delRow.getRowNum(), lastRow, -1);
-			
+
+			// row number
 			int idx = delRow.getRowNum();
-			while(sheet.getRow(idx) != null && sheet.getRow(idx).getCell(1) != null) {
+			while (sheet.getRow(idx) != null && sheet.getRow(idx).getCell(1) != null) {
 				Row row = sheet.getRow(idx);
-				idx+=2;
+
+				idx += 2;
 				String cellVal = dataFormatter.formatCellValue(row.getCell(1));
-				System.out.println("cell val bf =" + cellVal);
-				
 				cellVal = cellVal.substring(0, cellVal.indexOf('.'));
-				
-				System.out.println("cell val =" + cellVal);
 				row.getCell(1).setCellValue("" + (Integer.valueOf(cellVal) - 1) + ".");
 			}
-			
+
+			// row color
+			rows = sheet.rowIterator();
+			boolean rowToFill = false;
+			int isGrey = 0;
+			while (rows.hasNext()) {
+				Row row = rows.next();
+				Iterator<Cell> cells = row.cellIterator();
+				while (cells.hasNext()) {
+					Cell cell = cells.next();
+					CellAddress ca = cell.getAddress();
+					String cellVal = dataFormatter.formatCellValue(cell);
+					if (cellVal.equals("1.")) {
+						rowToFill = true;
+					}
+					if (rowToFill) {
+						System.out.print(dataFormatter.formatCellValue(cell) + "\t");	
+						if (ca.getColumn() >= 1 && ca.getColumn() <= 9) {
+							CellStyle cs = wb.createCellStyle();
+
+							if ((isGrey) % 2 != 0) {
+								cs.setFillBackgroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
+								cs.setFillPattern(FillPatternType.BIG_SPOTS);
+							} else {
+								cs.setFillPattern(FillPatternType.NO_FILL);
+							}
+							cell.setCellStyle(cs);
+						} else {
+							CellStyle cs = wb.createCellStyle();
+							cs.setFillPattern(FillPatternType.NO_FILL);
+							cell.setCellStyle(cs);
+						}
+					}
+				}
+				isGrey ++;
+				System.out.println();
+			}
+
 			wb.write(os);
 		} catch (FileNotFoundException e1) {
 			e1.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				wb.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-	}
-
-	public static void whiteCell(Workbook wb, Row row, String txt) {
-		CellStyle style = wb.createCellStyle();
-		style.setFillBackgroundColor(IndexedColors.WHITE.getIndex());
-		style.setFillPattern( FillPatternType.SOLID_FOREGROUND );
-		Cell cell = row.createCell((short) 1);
-		cell.setCellValue(txt);
-		cell.setCellStyle(style);
 	}
 }
